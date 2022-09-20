bb9b48a3e76e1cec06fd183fdbe3ba3d9fb13f7b
Tyler Main <tyler.l.main@gmail.com>
2022-09-19 23:20:48 -0500

    tests passing

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
 'use strict';
 
-const express = require('express');
-const notFound = require('./error-handlers/404');
-console.log('we are working');
+const { start } = require('./src/app');
 
-// design principle: singleton
-const app = express();
-
-app.get('/', (req, res, next) => {
-  res.status(200).send('Hello World');
-});
-
-app.get('/bad', (req, res, next) => {
-  next('this is a bad route');
-});
-
-app.use('*', notFound);
-
-
-
-app.listen(3002);
+start();
